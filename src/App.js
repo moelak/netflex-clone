70b192cee783a75bf0f54390917d91d7900f3a3d
moe <moe.lak20@gmail.comgit config --global user.email moe.lak20@gmail.com>
2021-01-19 23:13:09 -0800

    adding the rows and the header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React from 'react';
 import './App.css';
 import Row from './components/Row';
 import requests from './requests';
+import Banner from './components/Banner';
 
 function App() {
 	return (
 		<div className="App">
+			<Banner />
 			<Row
 				title="NETFLIX ORIGINALS"
 				fetchUrl={requests.fetchNetflixOriginals}
+				isLargeRow={true}
 			/>
 			<Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+
+			<Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+
+			<Row title="ACTION Movies" fetchUrl={requests.fetchActionMovies} />
+
+			<Row title="COMEDY Movies" fetchUrl={requests.fetchComedyMovies} />
+
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+			<Row
+				title="Documentaries Movies"
+				fetchUrl={requests.fetchDocumentaries}
+			/>
 		</div>
 	);
 }
--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../../axios';
+import './style.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
 	const [movies, setMovies] = useState([]);
 
 	useEffect(() => {
 		async function fetchData() {
 			const request = await axios.get(fetchUrl);
-			// console.log(request.data.results);
 			setMovies(request.data.results);
 			return request;
 		}
 		fetchData();
 	}, [fetchUrl]);
-
-	// console.log('test', movies);
 	// console.log(movies);
 
 	return (
-		<div>
+		<div className="row">
 			<h2>{title}</h2>
 
-			<div>
-				{movies.map((movie, index) => (
+			<div className="row__posters">
+				{movies.map(movie => (
 					<img
-						key={index}
-						src={`${base_url}${movie.poster_path}`}
+						className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+						key={movie.id}
+						src={`${base_url}${
+							isLargeRow ? movie.poster_path : movie.backdrop_path
+						}`}
 						alt={movie.name}
 					/>
 				))}
